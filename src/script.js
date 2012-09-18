432c2942f7671d85082fb93416bb72129c8ea4e2
Vlad Maraev <vlad.maraev@me.com>
2012-09-18 18:39:18 +0400

    e4x everywhere

--- src/script.js
+++ src/script.js
@@ -1,28 +1,29 @@
 function searchAni(d, client_ani) {
   for (i = 0; i < d.client.length(); i++) {
     if (client_ani == d.client[i].ani) {
-      return d.client[i].card-id;
+      return d.client[i].child("card-id");
     }
   }
   return -1;
 }
 
-function getCardId (xml) {
-  var status = xml.getElementsByTagName('Enroll').item(0).getAttribute('Status');
-  if (status == 'OK') {
-    return xml.getElementsByTagName('Enroll').item(0).getAttribute('CardID');
+function getCardId (d)
+  {
+    var status = d.@Status;
+    if (status == 'OK') {
+      return d.@CardID;
+    }
+    return -1;
   }
-  return -1;
-}
 
 function clientJson (client_ani,client_card_id) {
   return {client: {ani: client_ani, card_id: client_card_id}};
 }
 
-function getVerificationScore (xml) {
-  var status = xml.getElementsByTagName('EnrollVerify').item(0).getAttribute('Status');
+function getVerificationScore (d) {
+  var status = d.@EnrollVerify;
   if (status == 'OK') {
-    var score = xml.getElementsByTagName('VoiceKeyScore').item(0).firstChild.data;
+    var score = d.VoiceKeyScore;
     return score;
   }
   return -1;
